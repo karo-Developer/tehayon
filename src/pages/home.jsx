6db56c4941660250b5928d8d7bd99104dca6cb5e
karo-Developer <karographics21@gmail.com>
2024-11-13 01:14:23 +0000

    link

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -104,14 +104,16 @@ const Home = () => {
 
 
         <div className=''>
-          <span className='text-white p-5 cursor-pointer hover:border-b-4 border-white transition-all'> <a href="http://localhost:5173/gallery">Gallery</a></span>
+          <span className='text-white p-5 cursor-pointer hover:border-b-4 border-white transition-all'> 
           <Link to="/gallery">Gallery</Link>
-          <span className='text-white p-5 cursor-pointer hover:border-b-4 border-white transition-all'> <a href="http://localhost:5173/contact">Contact</a></span>
-          <Link to="/contact">Contact</Link>
-          <span className='text-white p-5 cursor-pointer hover:border-b-4 border-white transition-all'> <a href="http://localhost:5173/contact">Mail</a></span>
-          <Link to="/contact">Contact</Link>
-          <span className='text-white p-5 cursor-pointer hover:border-b-4 border-white transition-all'> <a href="http://localhost:5173/about"> Service</a></span>
-          <Link to="/about">about</Link>
+
+          </span>
+          <span className='text-white p-5 cursor-pointer hover:border-b-4 border-white transition-all'>           <Link to="/contact">Contact</Link>
+          </span>
+          <span className='text-white p-5 cursor-pointer hover:border-b-4 border-white transition-all'>           <Link to="/contact">Contact</Link>
+          </span>
+          <span className='text-white p-5 cursor-pointer hover:border-b-4 border-white transition-all'>           <Link to="/about">about</Link>
+          </span>
 
         </div>
 
